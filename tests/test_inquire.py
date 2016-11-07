e383b6907bd0a0a49145a8a1182d62438a91efdf
Fedor Baart <fedor.baart@deltares.nl>
2016-11-07 22:54:30 +0100

    fail tests that we expect to work

--- tests/test_inquire.py
+++ tests/test_inquire.py
@@ -15,21 +15,22 @@ f34_file = f34_file
 
 logger = logging.getLogger(__name__)
 
-
 def test_inqcel(f34_file):
-    cel_name = 'Cell 1'
-    count = 2
+    cel_name = 'map-const'
+    count = 41
     error, count, elm_names = nefis.cnefis.inqcel(f34_file, cel_name, count)
     log_error(error)
+    assert error == 0, "Error should be 0 in inqcel"
 
 
 def test_inqdat(f34_file):
-    grp_name = 'Group 1'
+    grp_name = 'map-const'
     error, grp_defined = nefis.cnefis.inqdat(f34_file, grp_name)
-
+    log_error(error)
+    assert error == 0, "Error should be 0 in inqdat"
 
 def test_inqelm(f34_file):
-    elm_name = 'Element 1'
+    elm_name = 'SIMDAT'
     elm_dimensions = np.zeros(5, dtype='int32')
 
     (
@@ -41,16 +42,22 @@ def test_inqelm(f34_file):
         elm_description,
         elm_count
     ) = nefis.cnefis.inqelm(f34_file, elm_name, elm_dimensions)
+    log_error(error)
+    assert error == 0, "Error should be 0 in inqelm"
 
 
 def test_inqfcl(f34_file):
     count = 1
     error, cel_name, count, bytes, elm_names = nefis.cnefis.inqfcl(f34_file)
+    log_error(error)
+    assert error == 0, "Error should be 0 in inqfcl"
 
 
 def test_inqncl(f34_file):
     count = 2
     error, cel_name, count, bytes, elm_names = nefis.cnefis.inqncl(f34_file)
+    log_error(error)
+    assert error == 0, "Error should be 0 in inqncl"
 
 
 def test_inqfel(f34_file):
@@ -67,6 +74,8 @@ def test_inqfel(f34_file):
         elm_size,
         elm_count
     ) = nefis.cnefis.inqfel(f34_file, count, elm_dimensions)
+    log_error(error)
+    assert error == 0, "Error should be 0 in inqfel"
 
 
 def test_inqnel(f34_file):
@@ -83,6 +92,8 @@ def test_inqnel(f34_file):
         elm_size,
         elm_count
     ) = nefis.cnefis.inqnel(f34_file, elm_count_dimensions, elm_dimensions)
+    log_error(error)
+    assert error == 0, "Error should be 0 in inqnel"
 
 
 def test_inqfgr(f34_file):
@@ -99,7 +110,7 @@ def test_inqfgr(f34_file):
 
 
 def test_inqngr(f34_file):
-    grp_count_dimensions = 0
+    grp_count_dimensions = 1
     grp_dimensions = np.zeros(5, dtype='int32')
     grp_order = np.zeros(5, dtype='int32')
 
@@ -109,44 +120,62 @@ def test_inqngr(f34_file):
         cel_name,
         grp_dim_count
     ) = nefis.cnefis.inqngr(f34_file, grp_count_dimensions, grp_dimensions, grp_order)
+    log_error(error)
+    assert error == 0, "Error should be 0 in inqngr"
 
 
 def test_inqfia(f34_file):
-    grp_name = 'Group 1'
+    grp_name = 'map-const'
     error, att_name, att_value = nefis.cnefis.inqfia(f34_file, grp_name)
+    log_error(error)
+    assert error == 0, "Error should be 0 in inqfia"
 
 
 def test_inqnia(f34_file):
-    grp_name = 'Group 1'
+    grp_name = 'map-const'
     error, att_name, att_value = nefis.cnefis.inqnia(f34_file, grp_name)
+    log_error(error)
+    assert error == 0, "Error should be 0 in inqnia"
 
 
 def test_inqfra(f34_file):
-    grp_name = 'Group 1'
+    grp_name = 'map-const'
     error, att_name, att_value = nefis.cnefis.inqfra(f34_file, grp_name)
+    log_error(error)
+    assert error == 0, "Error should be 0 in inqfra"
 
 
 def test_inqnra(f34_file):
-    grp_name = 'Group 1'
+    grp_name = 'map-const'
     error, att_name, att_value = nefis.cnefis.inqnra(f34_file, grp_name)
+    log_error(error)
+    assert error == 0, "Error should be 0 in inqnra"
 
 
 def test_inqfsa(f34_file):
-    grp_name = 'Group 1'
+    grp_name = 'map-const'
     error, att_name, att_value = nefis.cnefis.inqfsa(f34_file, grp_name)
+    log_error(error)
+    assert error == 0, "Error should be 0 in inqfsa"
 
 
 def test_inqnsa(f34_file):
-    grp_name = 'Group 1'
+    grp_name = 'map-const'
     error, att_name, att_value = nefis.cnefis.inqnsa(f34_file, grp_name)
+    log_error(error)
+    assert error == 0, "Error should be 0 in inqnsa"
 
 
 def test_inqfst(f34_file):
     error, grp_name, grp_defined = nefis.cnefis.inqfst(f34_file)
+    log_error(error)
+    assert error == 0, "Error should be 0 in inqfst"
 
 
 def test_inqnxt(f34_file):
     error, grp_name, grp_defined = nefis.cnefis.inqnxt(f34_file)
+    log_error(error)
+    assert error == 0, "Error should be 0 in inqnxt"
 
 
 def test_inqgrp(f34_file):
@@ -155,8 +184,12 @@ def test_inqgrp(f34_file):
     grp_dimensions = np.zeros(5, dtype='int32')
     grp_order = np.arange(1, 6, dtype='int32')
     error, cel_name, grp_count = nefis.cnefis.inqgrp(f34_file, grp_defined, grp_count, grp_dimensions, grp_order)
+    log_error(error)
+    assert error == 0, "Error should be 0 in inqgrp"
 
 
 def test_inqmxi(f34_file):
-    grp_name = 'Group 1'
+    grp_name = 'map-const'
     error, max_index = nefis.cnefis.inqmxi(f34_file, grp_name)
+    log_error(error)
+    assert error == 0, "Error should be 0 in inqmxi"
